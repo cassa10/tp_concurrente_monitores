f8dd1132b5a78a3f1c72feb6ad9f18042302ba09
cassa10 <cassanojoseluis@gmail.com>
2019-06-01 01:44:45 -0300

    Agrego metodo en Threadpool

--- java/src/ThreadPool.java
+++ java/src/ThreadPool.java
@@ -8,33 +8,37 @@ public class ThreadPool {
     private int workersNumber;
     private List<Barrier> barriers;
 
-    public ThreadPool(Buffer<Task> buffer, int workersNumber){
+    public ThreadPool(Buffer<Task> buffer, int workersNumber) {
 
         this.workersNumber = workersNumber;
         this.buffer = buffer;
         this.barriers = new ArrayList<>();
 
-        for(int i=0;i<workersNumber;i++) {
-             new Worker(buffer).start();
+        for (int i = 0; i < workersNumber; i++) {
+            new Worker(buffer).start();
         }
     }
 
     //NO USAR POISON PILL CON ESTE METODO YA QUE NO POSEE BARRERA
-    public void launch(Task tarea){
+    public synchronized void launch(Task tarea) {
         barriers.add(new Barrier(1));
         buffer.writeBuffer(tarea);
     }
 
-    public void stop(){
+    public synchronized void stop() {
         //TODO
         // NOSE SI AGREGA SOLO UNA TAREA POISONPILL
         // TIENE QUE IR UNA BARRERA? PARA QUE TERMINEN TODOS LOS WORKERS ANTES DE PONER LA POSION
 
-        barriers.forEach(b -> b.esperar());
+        barriers.forEach(b -> this.passBarrier(b));
         buffer.writeBuffer(new TaskPoisonPill());
     }
 
     //TODO
     //  PROBLEMA DEL BARRIER EL LAUNCH() TIENE PRIORIDAD ANTE EL STOP().
 
+    private void passBarrier(Barrier barrier){
+        barrier.esperar();
+        this.barriers.remove(barrier);
+    }
 }
